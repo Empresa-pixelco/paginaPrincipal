6fd1ebbaa6b6eac90f5bf4194caf0d60661479b4
Lucas <lucasbasoaltocontreras@gmail.com>
2023-04-26 18:11:57 -0400

    ultima seccion

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -15,9 +15,9 @@ export class LoginComponent implements OnInit {
   location = inject(Location);
   router = inject(Router);
   
-  email: string | any;
-  password: string | any;
-  constructor(private authService: AuthService) {}
+  // email: string | any;
+  // password: string | any;
+  // constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -31,20 +31,20 @@ export class LoginComponent implements OnInit {
 
   
 
-  onSubmit() {
+  // onSubmit() {
     
-    // Llama al servicio de autenticación para realizar el inicio de sesión
-    this.authService.login(this.email, this.password).subscribe(
-      // Maneja la respuesta del API de autenticación
-      (response) => {
-        // Guarda el token JWT en el local storage o en una cookie
-        const token = response.token;
-        // Haz lo que necesites con el token, por ejemplo, redirigir a otra página
-      },
-      (error) => {
-        // Maneja el error de autenticación
-        console.error('Error de autenticación:', error);
-      }
-    );
-  }
+  //   // Llama al servicio de autenticación para realizar el inicio de sesión
+  //   this.authService.login(this.email, this.password).subscribe(
+  //     // Maneja la respuesta del API de autenticación
+  //     (response) => {
+  //       // Guarda el token JWT en el local storage o en una cookie
+  //       const token = response.token;
+  //       // Haz lo que necesites con el token, por ejemplo, redirigir a otra página
+  //     },
+  //     (error) => {
+  //       // Maneja el error de autenticación
+  //       console.error('Error de autenticación:', error);
+  //     }
+  //   );
+  // }
 }
