78f642ec685ab88414f3d8d239e6b21756604935
Lucas <lucasbasoaltocontreras@gmail.com>
2023-05-10 20:09:16 -0400

    ultima seccion

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
+
 export const environment = {
-    production: true,
-    apiUrl: 'http://ultravetshop.cl:3003', // URL del backend
+    apiUrl: 'http://149.100.142.70:3003', // URL del backend
     key: 'df6147b10517f518d4ef7631e34117dc13d7bd5fadc7b75c5f8efa4ca40ea356',
-    iv: 'ed29dd55c39d48e95dab69bb26584f86'
+    iv: 'ed29dd55c39d48e95dab69bb26584f86',
 
     // const keyHex: CryptoJS.enc.Hex.stringify(key),
     // const ivHex: CryptoJS.enc.Hex.stringify(iv),
